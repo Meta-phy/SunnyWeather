cceb67dad92b85f992a81f6db002d62aec378e8a
Metaphy <15626993613@163.com>
2021-06-07 14:45:40 +0800

    添加locationId

--- app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.kt
+++ app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.kt
@@ -4,7 +4,14 @@ import com.google.gson.annotations.SerializedName
 
 data class PlaceResponse(val code: String, val location: List<Place>)
 data class Place(
-    val name: String, val location: Location,val lat: String ,val lon: String,val country: String,val adm1: String,val adm2: String,
+    val name: String,
+    val location: Location,
+    val lat: String,
+    val lon: String,
+    val country: String,
+    val adm1: String,
+    val adm2: String,
+    val id:String,
     @SerializedName("formatted_address") val address: String
 )
 
