e32714b9460cdfcac29d0438fdba0942f38522d0
Metaphy <15626993613@163.com>
2021-06-10 11:43:22 +0800

    新增切换城市

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
@@ -12,41 +12,59 @@ import com.example.sunnyweather.R
 import com.example.sunnyweather.WeatherActivity
 import com.example.sunnyweather.logic.model.Location
 import com.example.sunnyweather.logic.model.Place
+import kotlinx.android.synthetic.main.activity_weather.*
 
-class PlaceAdapter(private val fragment: Fragment, private val placeList: List<Place>) :
+class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: List<Place>) :
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) { val placeName: TextView = view.findViewById(
-        R.id.placeName)
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val placeName: TextView = view.findViewById(
+            R.id.placeName
+        )
         val placeAddress: TextView = view.findViewById(R.id.placeAddress)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val place = placeList[position]
         //对location进行处理
-        Log.d("test","创建卡片")
+        Log.d("test", "创建卡片")
         Log.d("test", place.toString())
-        place.location = Location(place.id,place.lon,place.lat)
+        place.location = Location(place.id, place.lon, place.lat)
         //
         holder.placeName.text = place.name
-        holder.placeAddress.text = place.country+" "+place.adm1+" "+place.adm2
+        holder.placeAddress.text = place.country + " " + place.adm1 + " " + place.adm2
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item,
-            parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.place_item,
+            parent, false
+        )
         val holder = ViewHolder(view)
         holder.itemView.setOnClickListener {
             val position = holder.adapterPosition
             val place = placeList[position]
-            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
-                putExtra("location_lng", place.lon)
-                putExtra("location_lat", place.lat)
-                putExtra("place_name", place.name)
-                putExtra("location_id", place.id)
+            val activity = fragment.activity
+            if (activity is WeatherActivity) {
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLng = place.lon
+                activity.viewModel.locationLat = place.lat
+                activity.viewModel.placeName = place.name
+                activity.viewModel.locationId = place.id
+                activity.refreshWeather()
+            } else {
+                val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                    putExtra("location_lng", place.lon)
+                    putExtra("location_lat", place.lat)
+                    putExtra("place_name", place.name)
+                    putExtra("location_id", place.id)
+                }
+                fragment.startActivity(intent)
+                fragment.activity?.finish()
             }
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
+            fragment.viewModel.savePlace(place)
         }
         return holder
     }
+
     override fun getItemCount() = placeList.size
 }
