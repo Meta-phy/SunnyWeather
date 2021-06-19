d0ac597fd8e75ed5b14d22bb0cdbe2e15b26086d
Metaphy <15626993613@163.com>
2021-06-19 18:00:20 +0800

    添加广播接收器

--- app/src/main/java/com/example/sunnyweather/WeatherActivity.kt
+++ app/src/main/java/com/example/sunnyweather/WeatherActivity.kt
@@ -1,9 +1,6 @@
 package com.example.sunnyweather
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.content.*
 import android.graphics.Color
 import android.os.Binder
 import android.os.Bundle
@@ -30,16 +27,22 @@ import kotlinx.android.synthetic.main.now.*
 
 class WeatherActivity : AppCompatActivity() {
     val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel.WeatherViewModel::class.java) }
-
+    lateinit var timeChangeReceiver: TimeChangeReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //设置顶部透明
         val decorView = window.decorView
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = Color.TRANSPARENT
 
-         window.statusBarColor = Color.TRANSPARENT
+        //注册广播
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.intent.action.TIME_TICK")
+        timeChangeReceiver = TimeChangeReceiver()
+        registerReceiver(timeChangeReceiver, intentFilter)
 
+        //加载数据
         setContentView(R.layout.activity_weather)
         if (viewModel.locationLng.isEmpty()) {
             viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
@@ -88,6 +91,18 @@ class WeatherActivity : AppCompatActivity() {
         })
 
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //取消注册广播
+        unregisterReceiver(timeChangeReceiver)
+    }
+    //监听时间变化,每分钟自动刷新天气
+    inner class TimeChangeReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            refreshWeather()
+        }
     }
 
     private fun showWeatherInfo(weather: Weather) {
